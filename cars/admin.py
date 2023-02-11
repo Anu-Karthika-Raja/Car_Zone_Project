882c3745484e463f1c830949a1b976146485000f
Anu-Karthika-Raja <anuhema0524@gmail.com>
2023-02-11 22:04:41 +0530

    Search Function Completed

--- cars/admin.py
+++ cars/admin.py
@@ -8,8 +8,8 @@ class CarAdmin(admin.ModelAdmin):
     def thumbnail(self, object):
         return format_html('<img src="{}" width="40" style="border-radius:45px"/>'.format(object.car_photo.url))
     thumbnail.short_description ='Car Image'
-    list_display = ('thumbnail','car_title','city', 'model', 'body_style', 'fuel_type','is_featured')
-    list_display_links = ('thumbnail','car_title')
+    list_display = ('id','thumbnail','car_title','city', 'model', 'body_style', 'fuel_type','is_featured')
+    list_display_links = ('id','thumbnail','car_title')
     list_editable = ('is_featured',)
     search_fields=('car_title','city', 'model', 'body_style')
     list_filter= ('city','model','fuel_type')
